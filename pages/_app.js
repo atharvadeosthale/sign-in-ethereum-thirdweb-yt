ad55d21a6f865a0fbd06b5f2151c72376b027879
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-09-21 19:28:15 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Mainnet}
+      authConfig={{
+        authUrl: "/api/auth",
+        domain: "localhost:3000",
+        loginRedirect: "/",
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
